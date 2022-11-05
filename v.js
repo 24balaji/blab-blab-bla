79fd3f44c1c98cd5734bf937a36fe82e60fa9875
24balaji <99334076+24balaji@users.noreply.github.com>
2022-11-05 18:48:25 +0530

    Add files via upload

---
+++ v.js
@@ -0,0 +1,69 @@
+
+img3="";
+status = "";
+objects = [];
+
+function preload(){
+  
+   img3= loadImage('vehicles.jfif');
+}
+
+function setup(){
+    canvas= createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw() 
+{
+    image(img3,0,0,640,420);
+
+    if(status != "")
+    {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(img3,gotResult);
+        for (i =0; i<objects.length; i++ )
+        {
+            document.getElementById('status').innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are" + objects.length;
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x + 15 ,objects[i].y + 15 );
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+        }   
+    }
+   
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status=true;
+    objectDetector.detect(img3, gotResult);
+}
+
+function gotResult(error,results){
+    if (error) {
+        console.log(error);
+    }else 
+    {
+       console.log(results);
+       objects =results;
+    }
+}
+
+function next()
+{
+    window.location="index.html";
+ }
+
+ function back()
+ {
+    window.location="tdevices.html";
+ }
